564d0ddbf29e840c8768fdcca5b3441c271c94d2
IhtishamShah <ihtisham.shah48@gmail.com>
2017-01-09 21:22:03 +0500

    Added ES6 support

--- Project/assets/js/index.js
+++ Project/assets/js/index.js
@@ -1,12 +1,49 @@
-var React = require('react')
-var ReactDOM = require('react-dom')
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+// var ReactDOM = require('react-dom')
+
+
+let questions = [
+	{
+		question: 'what dis?',
+		options: ['yes', 'no', 'brah'],
+		correct: 'yes'
+	},
+	{
+		question: 'overwatch',
+		options: ['pro', 'noob', 'pronoob'],
+		correct: 'noob'
+	}
+]
+
+const Question = ({question}) =>
+	<div>
+		{question.map((item)=>
+			<div>
+			<span>
+				{item.question}
+			</span>
+			<ul>
+				{item.options.map((opt)=>
+					<li>{opt}</li>
+				)}
+			</ul>
+			</div>
+		)}
+	</div>
+
+
 
 var Hello = React.createClass ({
     render: function() {
         return (
+        	<div>
             <h1>
             Hello, React! Bro!
             </h1>
+            <Question question={questions}/>
+            </div>
         )
     }
 })
