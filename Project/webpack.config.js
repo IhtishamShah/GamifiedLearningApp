d6da42f3c68575a74adc33cda6a28ac7fe7028dc
IhtishamShah <ihtisham.shah48@gmail.com>
2017-01-09 19:49:09 +0500

    reduced main.js size

--- Project/webpack.config.js
+++ Project/webpack.config.js
@@ -2,6 +2,8 @@ var path = require('path')
 var webpack = require('webpack')
 var BundleTracker = require('webpack-bundle-tracker')
 
+
+
 module.exports = {
     //the base directory (absolute path) for resolving the entry option
     context: __dirname,
@@ -9,23 +11,33 @@ module.exports = {
     //your current directory. You don't have to specify the extension  now,
     //because you will specify extensions later in the `resolve` section
     entry: './assets/js/index', 
-    
+    // devtool: 'cheap-module-source-map',
     output: {
         //where you want your compiled bundle to be stored
         path: path.resolve('./assets/bundles/'), 
         //naming convention webpack should use for your files
-        filename: '[name]-[hash].js', 
+        filename: 'main.js', 
     },
     
     plugins: [
         //tells webpack where to store data about your bundles.
         new BundleTracker({filename: './webpack-stats.json'}), 
+        // new webpack.optimize.CommonsChunkPlugin('common.js'),
+        new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.UglifyJsPlugin(),
+        new webpack.optimize.AggressiveMergingPlugin(),
         //makes jQuery available in every module
-        new webpack.ProvidePlugin({ 
-            $: 'jquery',
-            jQuery: 'jquery',
-            'window.jQuery': 'jquery' 
-        })
+        // new webpack.ProvidePlugin({ 
+        //     $: 'jquery',
+        //     jQuery: 'jquery',
+        //     'window.jQuery': 'jquery' 
+        // }),
+        new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+  })
+
     ],
     
     module: {
